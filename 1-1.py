8f98665d048c899f62f908c89ad5e52823886f46
kodama-tier4 <hideaki.kodama@tier4.jp>
2023-09-05 18:27:43 +0900

    条件式の大まかな設定

--- 1-1.py
+++ 1-1.py
@@ -14,7 +14,7 @@ a = 0.196  # 減速度
 td = 0.7  # 空走時間
 
 Dy = -10  # 歩行者がどれくらい車道から離れているか
-Dy_w = abs(-Dy - 2)  # 歩行者から見た、車と接触しない奥側の位置
+Dy_w = abs(Dy - 3)  # 歩行者から見た、車と接触しない奥側の位置
 Dx = x_p - x_e  # Egoと歩行者の位置
 
 tp = Dy / v0_p  # 歩行者が車道に到達する時間
@@ -24,26 +24,28 @@ tv = (
 s = v0_e ^ 2 / (2 * a) + v0_e * td  # 停止までの走行距離
 
 
-if (Dy < 0) and (v0_e > abs(Dx / Dy * v0_p)):  # （１）
-    for elem in root.iter("ParameterDeclaration"):
-        if elem.attrib["name"] == "v0_e":
-            elem.set("value", str(v0_e))
-        elif elem.attrib["name"] == "EgoS":
-            elem.set("value", str(x_e))
-        elif elem.attrib["name"] == "v0_p":
-            elem.set("value", str(v0_p))
-        elif elem.attrib["name"] == "PedestrianS":
-            elem.set("value", str(x_p))
-        elif elem.attrib["name"] == "Dy":
-            elem.set("value", str(Dy))
-elif (tp > tv) and (s > Dx):  # （２）
+if Dy < 1:  # (1〜2)
+    if (tp > tv) and (s > Dx):  # （2）
+        for elem in root.iter("ParameterDeclaration"):
+            if elem.attrib["name"] == "v0_e":
+                elem.set("value", str(v0_e))
+            elif elem.attrib["name"] == "EgoS":
+                elem.set("value", str(x_e))
+            elif elem.attrib["name"] == "v0_p":
+                elem.set("value", str(v0_p))
+            elif elem.attrib["name"] == "PedestrianS":
+                elem.set("value", str(x_p))
+            elif elem.attrib["name"] == "Dy":
+                elem.set("value", str(Dy))
+    elif tp > tv:  # (1)
+        pass
+elif (Dy >= 1) and (Dy < 3):  # (4)
     pass
-elif v0_e > Dx / Dy * v0_p:  # （３）
-    pass
-elif v0_e > Dx / Dy * v0_p:  # （４）
-    pass
-elif v0_e > Dx / Dy * v0_p:  # （５）
-    pass
+else:  # （5〜6）
+    if v0_e > Dx / Dy * v0_p:  # (5)
+        pass
+    elif v0_e > Dx / Dy * v0_p:  # (6)
+        pass
 
 
 # 特定の変数の内容を書き換える
