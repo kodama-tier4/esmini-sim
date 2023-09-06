13aea37b20f989662fe96f3953f3c38e0e36ab18
koutoriyabe <ko.toriyabe@tier4.jp>
2023-09-06 13:06:54 +0900

    移動

--- 1-1.py
+++ 1-1.py
@@ -2,11 +2,10 @@ import xml.etree.ElementTree as ET
 
 v0 = 10  # Egoの初速　単位はm/s
 vp = 50  # 歩行者の速度　単位はm/s
-Sx_p = 0  # 歩行者の初速
-Dy = 10
-B2y = Dy - 2
+Dy = 10  # 歩行者がどれくらい車道から離れているか
+Dy_w = -Dy - 2
 
 
 # XMLファイルを読み込む
-tree = ET.parse("/home/hideakikodama/esmini/resources/tier4/1-1.xosc")
+tree = ET.parse("/home/hideakikodama/esmini/resources/tier4/1-2.xosc")
 root = tree.getroot()
