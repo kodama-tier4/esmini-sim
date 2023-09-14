5ed213b145f81b631dbb56b069830415c2cfc119
koutoriyabe <ko.toriyabe@tier4.jp>
2023-09-14 13:12:54 +0900

    if文の整理

--- 1-1.py
+++ 1-1.py
@@ -2,20 +2,20 @@ import xml.etree.ElementTree as ET
 import math
 
 # XMLファイルを読み込む
-tree = ET.parse("resources/esmini-sim/1-2.xosc")
-# tree = ET.parse("/home/kotoriyabe/esmini-1/resources/esmini-sim/1-2.xosc")
+# tree = ET.parse("resources/esmini-sim/1-2.xosc")
+tree = ET.parse("/home/kotoriyabe/esmini-1/resources/esmini-sim/1-2.xosc")
 
 root = tree.getroot()
 
-v0_e = 10  # Egoの初速　単位はm/s
+v0_e = 5  # Egoの初速　単位はm/s
 x_e = 0  # Egoの初期位置
 v0_p = 1  # 歩行者の速度　単位はm/s
-x_p = 40  # 歩行者の初期位置
+x_p = 30  # 歩行者の初期位置
 
 a = 2.5  # 減速度　単位はm/s^2 0.196?
 td = 0.7  # 空走時間
 
-Dy = 7  # 歩行者がどれくらい車道から離れているか
+Dy = -10  # 歩行者がどれくらい車道から離れているか
 Dy_w = abs(Dy - 3)  # 歩行者から見た、車と接触しない奥側の位置
 Dx = x_p - x_e  # Egoと歩行者の距離
 
@@ -26,12 +26,34 @@ tv = v0_e / a
 # )  # 車両が「減速度 a」、「空走時間 td」で減速してA’-B’で停止する時間
 s = v0_e * (tv + v0_e / a) / 2  # 停止までの走行距離
 
+
+
+
+
+
+
+
+
+
 timeflag1 = 100
 timeflag2 = 100
 print("歩行者が車道に到達する時間：tp", tp)
 print("車両が「減速度 a」、「空走時間 td」で減速してA’-B’で停止する時間：tv", tv)
 print("停止までの走行距離：s", s)
 print("Egoと歩行者の距離：Dx", Dx)
+
+for elem in root.iter("ParameterDeclaration"):
+    if elem.attrib["name"] == "v0_e":
+            elem.set("value", str(v0_e))
+    elif elem.attrib["name"] == "EgoS":
+            elem.set("value", str(x_e))
+    elif elem.attrib["name"] == "v0_p":
+            elem.set("value", str(v0_p))
+    elif elem.attrib["name"] == "PedestrianS":
+            elem.set("value", str(x_p))
+    elif elem.attrib["name"] == "Dy":
+            elem.set("value", str(Dy))
+
 
 
 if Dy < 1:  # (1〜3)
@@ -41,67 +63,74 @@ if Dy < 1:  # (1〜3)
         for elem in root.iter("ParameterDeclaration"):
             if elem.attrib["name"] == "timeflag2":
                 elem.set("value", str(timeflag2))
-            elif elem.attrib["name"] == "v0_e":
-                elem.set("value", str(v0_e))
-            elif elem.attrib["name"] == "EgoS":
-                elem.set("value", str(x_e))
-            elif elem.attrib["name"] == "v0_p":
-                elem.set("value", str(v0_p))
-            elif elem.attrib["name"] == "PedestrianS":
-                elem.set("value", str(x_p))
-            elif elem.attrib["name"] == "Dy":
-                elem.set("value", str(Dy))
             elif elem.attrib["name"] == "DistanceToStop":
                 elem.set("value", str(s))  # 止まるまでの距離
-    elif tp > tv:  # (1)
-        print("(1)を実施")
-        for elem in root.iter("ParameterDeclaration"):
-            if elem.attrib["name"] == "v0_e":
-                elem.set("value", str(v0_e))
-            elif elem.attrib["name"] == "EgoS":
-                elem.set("value", str(x_e))
-            elif elem.attrib["name"] == "v0_p":
-                elem.set("value", str(v0_p))
             elif elem.attrib["name"] == "PedestrianS":
                 elem.set("value", str(x_p))
-            elif elem.attrib["name"] == "Dy":
-                elem.set("value", str(Dy))
+            # elif elem.attrib["name"] == "v0_e":
+            #     elem.set("value", str(v0_e))
+            # elif elem.attrib["name"] == "EgoS":
+            #     elem.set("value", str(x_e))
+            # elif elem.attrib["name"] == "v0_p":
+            #     elem.set("value", str(v0_p))
+            # elif elem.attrib["name"] == "Dy":
+            #     elem.set("value", str(Dy))
+    
+    elif tp > tv:  # (1)
+        print("(1)を実施")
+        # for elem in root.iter("ParameterDeclaration"):
+            # if elem.attrib["name"] == "v0_e":
+            #     elem.set("value", str(v0_e))
+            # elif elem.attrib["name"] == "EgoS":
+            #     elem.set("value", str(x_e))
+            # elif elem.attrib["name"] == "v0_p":
+            #     elem.set("value", str(v0_p))
+            # elif elem.attrib["name"] == "PedestrianS":
+            #     elem.set("value", str(x_p))
+            # elif elem.attrib["name"] == "Dy":
+            #     elem.set("value", str(Dy))
+
+
     else:  # (3)
         print("(3)を実施")
         timeflag1 = td
         for elem in root.iter("ParameterDeclaration"):
             if elem.attrib["name"] == "timeflag1":
-                elem.set("value", str(timeflag1))
-            elif elem.attrib["name"] == "v0_e":
-                elem.set("value", str(v0_e))
-            elif elem.attrib["name"] == "EgoS":
-                elem.set("value", str(x_e))
-            elif elem.attrib["name"] == "v0_p":
-                elem.set("value", str(v0_p))
-            elif elem.attrib["name"] == "PedestrianS":
-                elem.set("value", str(x_p))
-            elif elem.attrib["name"] == "Dy":
-                elem.set("value", str(Dy))
+                elem.set("value", str(timeflag1))                
             elif elem.attrib["name"] == "TimeToStop":
-                elem.set("value", str(tv))  # 止まるまでの時間
+                elem.set("value", str(tv))  # 止まるまでの時間  
+
+            # elif elem.attrib["name"] == "v0_e":
+            #     elem.set("value", str(v0_e))
+            # elif elem.attrib["name"] == "EgoS":
+            #     elem.set("value", str(x_e))
+            # elif elem.attrib["name"] == "v0_p":
+            #     elem.set("value", str(v0_p))
+            # elif elem.attrib["name"] == "PedestrianS":
+            #     elem.set("value", str(x_p))
+            # elif elem.attrib["name"] == "Dy":
+            #     elem.set("value", str(Dy))
+
+
 elif (Dy >= 1) and (Dy <= 4):  # (4)
     print("(4)を実施")
     timeflag1 = td
     for elem in root.iter("ParameterDeclaration"):
         if elem.attrib["name"] == "timeflag1":
             elem.set("value", str(timeflag1))
-        elif elem.attrib["name"] == "v0_e":
-            elem.set("value", str(v0_e))
-        elif elem.attrib["name"] == "EgoS":
-            elem.set("value", str(x_e))
-        elif elem.attrib["name"] == "v0_p":
-            elem.set("value", str(v0_p))
-        elif elem.attrib["name"] == "PedestrianS":
-            elem.set("value", str(x_p))
-        elif elem.attrib["name"] == "Dy":
-            elem.set("value", str(Dy))
         elif elem.attrib["name"] == "TimeToStop":
             elem.set("value", str(tv))  # 止まるまでの時間
+        # elif elem.attrib["name"] == "v0_e":
+        #     elem.set("value", str(v0_e))
+        # elif elem.attrib["name"] == "EgoS":
+        #     elem.set("value", str(x_e))
+        # elif elem.attrib["name"] == "v0_p":
+        #     elem.set("value", str(v0_p))
+        # elif elem.attrib["name"] == "PedestrianS":
+        #     elem.set("value", str(x_p))
+        # elif elem.attrib["name"] == "Dy":
+        #     elem.set("value", str(Dy))
+
 else:  # （5〜6）
     if (Dy > 4) and (Dy <= 6):  # (5)
         print("(5)を実施")
@@ -109,31 +138,32 @@ else:  # （5〜6）
         for elem in root.iter("ParameterDeclaration"):
             if elem.attrib["name"] == "timeflag2":
                 elem.set("value", str(timeflag2))
-            elif elem.attrib["name"] == "v0_e":
-                elem.set("value", str(v0_e))
-            elif elem.attrib["name"] == "EgoS":
-                elem.set("value", str(x_e))
-            elif elem.attrib["name"] == "v0_p":
-                elem.set("value", str(v0_p))
-            elif elem.attrib["name"] == "PedestrianS":
-                elem.set("value", str(x_p))
-            elif elem.attrib["name"] == "Dy":
-                elem.set("value", str(Dy))
             elif elem.attrib["name"] == "DistanceToStop":
                 elem.set("value", str(s))  # 止まるまでの距離
+            # elif elem.attrib["name"] == "v0_e":
+            #     elem.set("value", str(v0_e))
+            # elif elem.attrib["name"] == "EgoS":
+            #     elem.set("value", str(x_e))
+            # elif elem.attrib["name"] == "v0_p":
+            #     elem.set("value", str(v0_p))
+            # elif elem.attrib["name"] == "PedestrianS":
+            #     elem.set("value", str(x_p))
+            # elif elem.attrib["name"] == "Dy":
+            #     elem.set("value", str(Dy))
+
     else:  # (6)
         print("(6)を実施")
-        for elem in root.iter("ParameterDeclaration"):
-            if elem.attrib["name"] == "v0_e":
-                elem.set("value", str(v0_e))
-            elif elem.attrib["name"] == "EgoS":
-                elem.set("value", str(x_e))
-            elif elem.attrib["name"] == "v0_p":
-                elem.set("value", str(v0_p))
-            elif elem.attrib["name"] == "PedestrianS":
-                elem.set("value", str(x_p))
-            elif elem.attrib["name"] == "Dy":
-                elem.set("value", str(Dy))
+        # for elem in root.iter("ParameterDeclaration"):
+        #     if elem.attrib["name"] == "v0_e":
+        #         elem.set("value", str(v0_e))
+        #     elif elem.attrib["name"] == "EgoS":
+        #         elem.set("value", str(x_e))
+        #     elif elem.attrib["name"] == "v0_p":
+        #         elem.set("value", str(v0_p))
+        #     elif elem.attrib["name"] == "PedestrianS":
+        #         elem.set("value", str(x_p))
+        #     elif elem.attrib["name"] == "Dy":
+        #         elem.set("value", str(Dy))
 
-tree.write("resources/esmini-sim/1-3.xosc")
-# tree.write("/home/kotoriyabe/esmini-1/resources/esmini-sim/1-3.xosc")
+# tree.write("resources/esmini-sim/1-3.xosc")
+tree.write("/home/kotoriyabe/esmini-1/resources/esmini-sim/1-3.xosc")
